c4eedd912fd11c91261e0dae23e0f296673dfa5a
James Yang <gn00999829@gmail.com>
2021-02-12 21:01:35 -0800

    final

--- server.js
+++ server.js
@@ -13,16 +13,6 @@ server.use(express.json({ extended: false }));
 server.use("/", require("./routes/redirect"));
 server.use("/api/url", require("./routes/url"));
 
-// // Serve static assets in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   server.use(express.static("client/build"));
-
-//   server.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
 server.listen(PORT, () => {
   console.log(`server started at port ${PORT}`);
 });
